64b25a06c65997bc29d47a85c451c115a71ed675
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-11-13 16:11:16 +0100

    added javadoc (trigger travis)

---
+++ core/src/main/java/io/github/mmm/base/compare/package-info.java
@@ -0,0 +1,7 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Contains {@link io.github.mmm.base.compare.CompareOperator} and {@link io.github.mmm.base.compare.NumberComparator}
+ * that help to compare values.
+ */
+package io.github.mmm.base.compare;
