dafa54b2a310e58e006430ca3b8e829e20a4baa8
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-12-15 19:07:30 +0100

    added missing package-info

---
+++ core/src/main/java/io/github/mmm/base/text/package-info.java
@@ -0,0 +1,6 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Contains helpers to deal with text (string, etc.) and case.
+ */
+package io.github.mmm.base.text;
